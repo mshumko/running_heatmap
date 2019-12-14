494c3c43703c856bbe812470796493fc4652c28a
Mykhaylo Shumko <msshumko@gmail.com>
2019-12-13 19:26:21 -0800

    heatmap.py - moved methods around and added a center kwarg to __init__()
    README.md - second pass at writing a thorough readme file.
    Added a screenshot to demonstate how the map looks like.

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # running_heatmap
+If you are curious on how to implement Strava's simple, yet rich [heatmap](www.strava.com/heatmap), this repository will help you get started. I show a straight-forward implementation of a heatmap using your own running tracks in gpx format which is inspired by ```geo-heatmap```. 
 
-This project replicates Strava's [heat map](www.strava.com/heatmap) and plots my runs in and around Bozeman. The heatmap is generated with the following steps:
+An example showing various running routes around Bozeman, Montana (the output html code is interactive)
+![Bozeman heatmap](https://github.com/mshumko/running_heatmap/images/heatmap_1.jpg "Test")
 
-1. Install the gpxpy, folium, and pandas using 
+
+## Steps to make your own heatmap
+1. The two essential libraries are gpxpy for processing your gpx files; and folium, a wrapper for the Leaflet.js Javascript library. You can install these two libraries along with the standard numpy, pandas, etc. libraries with
 
 ```sudo pip3 install -r requirements.txt ```
 
-2. Obtain your gpx files that you want to map and place the gpx files in the /data/ folder in the base repo directory. Strava can export data but data manipulation is necessary. It is super easy to export data from Garmin via [garmin-connext-export](https://github.com/pe-st/garmin-connect-export).
+2. Download the gpx files that you want to map and save them in ```./data/```. You can get the data from Strava, but extra data manipulation is necessary. Alternatively, Garmin can provide you with all of your gpx files. Garmin Connect only allows you to manually download one track at a time, but it can be automated with the [garmin-connext-export](https://github.com/pe-st/garmin-connect-export) script. 
 
-3. Run heatmap.py to generate a heatmap file in ./data/heatmap.csv and heatmap.html
+3. After you have the gpx data, you will make the heatmap with heatmap.py which will generate and save a 2d heatmap historam to  ```./data/heatmap.csv``` and use it to make a heatmap saved in ```./data/heatmap.html```. 
 
-    Example:
-    ```python
-    h = Heatmap()
-    h.make_heatmap_hist() # No need to run this if heatmap.csv is already generated. Run h.load_heatmap()
-    h.make_map()
-    ```
+ - Initialize the ```Heatmap``` object with the ```center``` kwarg that specifies the map center, well as the ```lat_bins``` and ```lon_bins``` that define the 2d "heat" histogram. If you don't specify these parameters the program assumes you live in Bozeman.
+ - Histrogram all of the gpx track files in ```./data/``` using the ```make_heatmap_hist()``` method. Optionally you can set the ```save_heatmap``` (true by default) kwarg to save the data to avoid the time consuming gpx track every time. 
+ - Use the 2d histogram and folium to make a heatmap with the ```make_map()``` method. You will generate a ```heatmap.html``` file in the data directory.
+ - Drag the html file into a browser and enjoy! These are many useful kwargs to pass to the ```make_map()``` method to customise the heatmap to your liking.
 
-    Open heatmap.html in your browser and enjoy.
+Example:
+```python
+h = Heatmap(center=[-111.0329, 45.660])
+h.make_heatmap_hist() # Run h.load_heatmap() instead if heatmap.csv is generated. 
+h.make_map()
+```
--- heatmap.py
+++ heatmap.py
@@ -14,9 +14,23 @@ import gpxpy
 import gpxpy.gpx
 
 class Heatmap:
-    def __init__(self, lat_bins=None, lon_bins=None, box_width=0.4, grid_res=500):
-        """ 
+    def __init__(self, lat_bins=None, lon_bins=None, center=None, box_width=0.4, grid_res=500):
+        """
         Initialize the heatmap class and the latitude and longitude bins. 
+
+        Parameters
+        ----------
+        lat_bins : float array, optional
+            Latitude histogram bins.
+        lon_bins : float array, optional
+            Longitude histogram bins.
+        box_width : float
+            The size of the his
+
+        Returns
+        -------
+        bool
+            True if successful, False otherwise.      
         """
         self.grid_res = grid_res
         if (lat_bins is None) or (lon_bins is None):
@@ -29,49 +43,11 @@ class Heatmap:
         else:
             self.lon_bins = lon_bins
             self.lat_bins = lat_bins
+            self.center = center
 
         if not os.path.exists('./data/'):
             os.makedirs('./data/')
             print('Made empty data directory.')
-        return
-    
-    def make_map(self, blur_sigma=0.5, map_zoom_start=11, heatmap_radius=15,
-                    heatmap_blur=15, heatmap_min_opacity=0.5,
-                    heatmap_max_zoom=14):
-        """ 
-        Make a heatmap html file using folium
-        """
-        if not hasattr(self, 'heatmap'):
-            raise AttributeError('self.heatmap not found. Either run'
-                                ' the make_heatmap_hist() or '
-                                'load_heatmap() methods.')
-        # Make a terrain map.
-        self.m = folium.Map(location=self.center[::-1],
-                       zoom_start=map_zoom_start,
-                       tiles='Stamen Terrain', max_zoom=heatmap_max_zoom)
-
-        # Overlay the heatmap
-        latlat, lonlon = np.meshgrid(
-                                    self.heatmap.index, 
-                                    self.heatmap.columns.astype(float)
-                                    )
-        # idx boolean array necessary to not include lat-lon bins 
-        # with 0 gpx points because you can see all those points
-        # on the map.
-        idx = np.where(self.heatmap.values)
-        data = np.stack([latlat[idx].flatten(), 
-                        lonlon[idx].flatten(), 
-                        self.heatmap.values[idx].flatten()], 
-                        axis=-1)
-        # Add the heatmap with many argument tweaks.
-        heatmap = folium.plugins.HeatMap(data,
-                         max_val=self.heatmap.values.max(),
-                          min_opacity=heatmap_min_opacity,
-                          radius=heatmap_radius,
-                          blur=heatmap_blur,
-                          max_zoom=heatmap_max_zoom)
-        self.m.add_child(heatmap)
-        self.m.save('./data/heatmap.html')
         return
 
     def make_heatmap_hist(self, gpx_path='./data/', save_heatmap=True, verbose=False):
@@ -112,6 +88,45 @@ class Heatmap:
                                     index=self.lat_bins[:-1], 
                                     columns=self.lon_bins[:-1])
         if save_heatmap: self._save_heatmap()
+        return
+    
+    def make_map(self, blur_sigma=0.5, map_zoom_start=11, heatmap_radius=15,
+                    heatmap_blur=15, heatmap_min_opacity=0.5,
+                    heatmap_max_zoom=14):
+        """ 
+        Make a heatmap html file using folium
+        """
+        if not hasattr(self, 'heatmap'):
+            raise AttributeError('self.heatmap not found. Either run'
+                                ' the make_heatmap_hist() or '
+                                'load_heatmap() methods.')
+        # Make a terrain map.
+        self.m = folium.Map(location=self.center[::-1],
+                       zoom_start=map_zoom_start,
+                       tiles='Stamen Terrain', max_zoom=heatmap_max_zoom)
+
+        # Overlay the heatmap
+        latlat, lonlon = np.meshgrid(
+                                    self.heatmap.index, 
+                                    self.heatmap.columns.astype(float)
+                                    )
+        # idx boolean array necessary to not include lat-lon bins 
+        # with 0 gpx points because you can see all those points
+        # on the map.
+        idx = np.where(self.heatmap.values)
+        data = np.stack([latlat[idx].flatten(), 
+                        lonlon[idx].flatten(), 
+                        self.heatmap.values[idx].flatten()], 
+                        axis=-1)
+        # Add the heatmap with many argument tweaks.
+        heatmap = folium.plugins.HeatMap(data,
+                         max_val=self.heatmap.values.max(),
+                          min_opacity=heatmap_min_opacity,
+                          radius=heatmap_radius,
+                          blur=heatmap_blur,
+                          max_zoom=heatmap_max_zoom)
+        self.m.add_child(heatmap)
+        self.m.save('./data/heatmap.html')
         return            
 
     def load_heatmap(self, heatmap_path='./data/heatmap.csv'):
